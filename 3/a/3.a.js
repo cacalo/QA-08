a4f58af5b4a344b9e0757b4da902e1cd8ec6ae84
Cacalo <cacalo219@hotmail.com>
2021-03-24 13:06:17 -0300

    QA-08: Fixed exercise 3.A

--- 3/a/3.a.js
+++ 3/a/3.a.js
@@ -3,5 +3,5 @@
 mostrar por consola los meses 5 y 11 (utilizar console.log)*/
 
 months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-console.log(months[5]);
-console.log(months[11]);
+console.log(months[4]);
+console.log(months[10]);
