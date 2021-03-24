79b530e463706cf48414862edafb3ef4b05e75bb
Cacalo <cacalo219@hotmail.com>
2021-03-24 10:57:08 -0300

    Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).

--- 3/b/3.b.js
+++ 3/b/3.b.js
@@ -1,3 +1,2 @@
 months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-console.log(months[5]);
-console.log(months[11]);
+console.log(months.sort())
