95b901cb94f7507817f88074931a195b3f5ceab7
Cacalo <cacalo219@hotmail.com>
2021-03-24 12:36:16 -0300

    QA-08:5A

---
+++ 5/a/5.a.js
@@ -0,0 +1,8 @@
+/* Crear un array que contenga 5 palabras y recorrer dicho
+array utilizando un bucle for de JavaScript para mostrar una
+alerta utilizando cada una de las palabras.*/
+x=["1st","2nd","3rd"];
+for (let index = 0; index < x.length; index++) {
+    const element = x[index];
+    alert(element);
+}
